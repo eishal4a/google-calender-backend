2172c5552af32f3fcf9b27179e8112bf130e7456
Eishal <eishal.cecos@gmail.com>
2025-08-30 21:19:10 +0500

    p
    p

--- src/utils/googleApi.js
+++ src/utils/googleApi.js
@@ -8,3 +8,11 @@ export const oAuth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_SECRET,
   process.env.GOOGLE_REDIRECT_URI
 );
+
+// 🆕 Temporary global token store
+export let savedTokens = null;
+
+export function setTokens(tokens) {
+  savedTokens = tokens;
+  oAuth2Client.setCredentials(tokens);
+}
