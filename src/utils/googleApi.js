479496219806347fcf257ccf46a3d4110dbecad5
Eishal <eishal.cecos@gmail.com>
2025-08-30 21:30:25 +0500

    p
    p

--- src/utils/googleApi.js
+++ src/utils/googleApi.js
@@ -3,16 +3,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log("Google Config:", {
+  clientId: process.env.GOOGLE_CLIENT_ID,
+  secret: process.env.GOOGLE_CLIENT_SECRET ? "loaded" : "missing",
+  redirectUri: process.env.GOOGLE_REDIRECT_URI
+});
+
 export const oAuth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
   process.env.GOOGLE_REDIRECT_URI
 );
-
-// 🆕 Temporary global token store
-export let savedTokens = null;
-
-export function setTokens(tokens) {
-  savedTokens = tokens;
-  oAuth2Client.setCredentials(tokens);
-}
