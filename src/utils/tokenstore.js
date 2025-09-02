6cdc07dbab062d42f36ddad3652fb3fce3ff0278
Eishal <eishal.cecos@gmail.com>
2025-09-03 00:46:13 +0500

    Update tokenstore.js

--- src/utils/tokenstore.js
+++ src/utils/tokenstore.js
@@ -1,11 +1,14 @@
-// utils/tokenStore.js
-let storedTokens = null;
+import fs from "fs";
+const TOKEN_PATH = "./tokens.json";
 
+// Save Google tokens to file
 export function saveTokens(tokens) {
-  storedTokens = tokens;
+  fs.writeFileSync(TOKEN_PATH, JSON.stringify(tokens, null, 2));
   console.log("💾 Tokens saved:", Object.keys(tokens));
 }
 
+// Retrieve saved tokens
 export function getTokens() {
-  return storedTokens;
+  if (!fs.existsSync(TOKEN_PATH)) return null;
+  return JSON.parse(fs.readFileSync(TOKEN_PATH));
 }
